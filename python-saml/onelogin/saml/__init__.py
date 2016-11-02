f33ac10dbb4388301ddec3ff1b130b1b90b45922
Kai Xu <kai.xu@service-now.com>
2016-11-02 16:45:58 -0700

    add OneLogin's SAML Python Toolkit: python-saml

---
+++ python-saml/onelogin/saml/__init__.py
@@ -0,0 +1,9 @@
+from Response import (
+    Response,
+    ResponseValidationError,
+    ResponseNameIDError,
+    ResponseConditionError,
+    ResponseSubjectConfirmationError,
+    )
+import AuthRequest
+import SignatureVerifier
